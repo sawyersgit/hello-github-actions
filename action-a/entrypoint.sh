4796c4940beb9cfb0121dd6c589f9c65fb690737
sawyersgit <70953795+sawyersgit@users.noreply.github.com>
2020-10-13 12:26:31 -0400

    Grammar

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world, my name is $INPUT_MY_NAME. Let's begin our adventure and I'll let yu teach me to code."
+sh -c "echo Hello world, my name is $INPUT_MY_NAME. Let's begin our adventure and I will let you teach me to code, yoda."
