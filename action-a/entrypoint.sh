00bd30d90198545707ae2ed444bfa7c71a09c85e
sawyersgit <70953795+sawyersgit@users.noreply.github.com>
2020-10-13 12:22:47 -0400

    message update

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo Hello world, my name is $INPUT_MY_NAME. 
+Let's begin our adventure and I'll let yu teach me to code."
