e70dbe1b6959270f16bc1ff3b49270394132d177
sawyersgit <70953795+sawyersgit@users.noreply.github.com>
2020-10-13 13:45:29 -0400

    fix

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME Let's begin our adventure and I will let you teach me to code yoda";
+sh -c "echo Hello world my name is $INPUT_MY_NAME Let's begin our adventure and I will let you teach me to code yoda"
